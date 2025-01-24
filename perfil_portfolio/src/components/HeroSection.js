671e4616fb9a4a9c0e9dff1609514d64ee3f0e1b
DevFullFelps <devfullfelps@gmail.com>
2025-01-24 09:27:41 -0300

    att

--- perfil_portfolio/src/components/HeroSection.js
+++ perfil_portfolio/src/components/HeroSection.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import './HeroSection.css'
+import Typewriter from 'typewriter-effect';
+import './HeroSection.css';
 
 const HeroSection = () => {
-    return(
+    const scrollToContent = () => {
+        const content = document.getElementById('content');
+        content.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    return (
         <div className='hero-container'>
             <img src='../img_fundo_hero.jpg' alt='Background' className='hero-background'></img>
+            <div className='hero-overlay'></div>
             <div className='hero-content'>
                 <h4 className='hero-welcome'>Bem Vindo</h4>
                 <h1 className='hero-title'>
-                    <span className='typed-text'>Desenvolvedor WEB | Front-End</span>
-                    <span className='cursor'>&nbsp;</span>
+                    <Typewriter
+                        options={{
+                            strings: ['Desenvolvedor WEB | Front-End'],
+                            autoStart: true,
+                            loop: true,
+                            deleteSpeed: 50, // Velocidade ao apagar
+                            delay: 100, // Velocidade ao digitar
+                        }}
+                    />
                 </h1>
+                <div className="scroll-down" onClick={scrollToContent}>
+                    <div className="arrow"></div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HeroSection;
--- perfil_portfolio/src/components/Navbar.js
+++ perfil_portfolio/src/components/Navbar.js
@@ -1,45 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
-const Navbar = () =>{
-    return(
-        <nav className='navbar'>
-            {/* Seção da imagem e nome */}
-            <section className='navbar-header'> 
-                <div className='navbar-image-container'>
-                    <img src='../perfil_iguana.jpg' alt='Logo' className='navbar-image'/>
-                    <div class="loader">
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className={`navbar ${isOpen ? 'navbar-expanded' : ''}`}>
+            {/* Botão de hambúrguer */}
+            {/* Navbar para telas menores */}
+            
+            <div className="hamburger" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+      
+
+      {/* Dropdown para telas menores */}
+      <div className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <a href="#home" onClick={toggleMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#aboutme" onClick={toggleMenu}>
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#projects" onClick={toggleMenu}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={toggleMenu}>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">
+          Chamar no WhatsApp
+        </a>
+      </div>
+
+            {/* Dropdown - Links de navegação para telas menores */}
+            {isOpen && (
+                <section className="dropdown-menu">
+                    <ul>
+                        <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+                        <li><a href="#aboutme" onClick={toggleMenu}>About Me</a></li>
+                        <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+                        <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
+                    </ul>
+                    <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">Chamar no WhatsApp</a>
+                </section>
+            )}
+
+            {/* Navbar completa para telas maiores */}
+            <section className="navbar-header">
+                <div className="navbar-image-container">
+                    <img src="../perfil_iguana.jpg" alt="Logo" className="navbar-image" />
+                    <div className="loader">
                         <svg viewBox="0 0 80 80">
                             <circle r="32" cy="40" cx="40"></circle>
                         </svg>
                     </div>
                 </div>
-                <div className='navbar-name'>Felipe Adriano</div>
+                <div className="navbar-name">Felipe Adriano</div>
             </section>
-            {/* Links de navegação */}
-            <section className='navbar-link'>
+
+            <section className="navbar-link">
                 <ul>
-                    <li><a href='#home'>Home</a></li>
-                    <li><a href='#aboutme'>About Me</a></li>
-                    <li><a href='#projects'>Projects</a></li>
-                    <li><a href='#contact'>Contact</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#aboutme">About Me</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </section>
-            {/* Botão de Whatsapp */}
-            <section className='navbar-whatsapp'>
-                <a href='https://wa.me/seu-numerodo-whatsapp' className='whatsapp-button'>Chamar no WhatsApp</a>
+
+            <section className="navbar-whatsapp">
+                <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">Chamar no WhatsApp</a>
             </section>
-            {/* Seção das Logos */}
-            <section className='navbar-footer'>
+
+            <section className="navbar-footer">
                 <ul>
                     <li><a href="https://github.com/seu-perfil"><i className="fab fa-github"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/seu-perfil"><i className="fab fa-linkedin"></i></a></li>
                     <li><a href="https://www.instagram.com/seu-perfil"><i className="fab fa-instagram"></i></a></li>
                     <li><a href="https://twitter.com/seu-perfil"><i className="fab fa-twitter"></i></a></li>
                 </ul>
-            </section> 
+            </section>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
