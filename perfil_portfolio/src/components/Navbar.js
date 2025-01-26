26ad4be8e05d26d7b9a89ea77d602acefb024868
DevFullFelps <devfullfelps@gmail.com>
2025-01-26 09:01:09 -0300

    Atualização
    Navbar e hero section responsiva

--- perfil_portfolio/src/components/Navbar.js
+++ perfil_portfolio/src/components/Navbar.js
@@ -10,68 +10,29 @@ const Navbar = () => {
 
     return (
         <nav className={`navbar ${isOpen ? 'navbar-expanded' : ''}`}>
-            {/* Botão de hambúrguer */}
-            {/* Navbar para telas menores */}
-            
-            <div className="hamburger" onClick={toggleMenu}>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="hamburger-container">
+                <div className="hamburger" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="navbar-name-small">Felipe Adriano</div>
             </div>
 
-      
+            <div className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
+                <div className="navbar-name-small">Felipe Adriano</div>
+                <ul>
+                    <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+                    <li><a href="#aboutme" onClick={toggleMenu}>About Me</a></li>
+                    <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+                    <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
+                </ul>
+                <a href="https://wa.me/47984318419" className="whatsapp-button">Chamar no WhatsApp</a>
+            </div>
 
-      {/* Dropdown para telas menores */}
-      <div className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
-        <ul>
-          <li>
-            <a href="#home" onClick={toggleMenu}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#aboutme" onClick={toggleMenu}>
-              About Me
-            </a>
-          </li>
-          <li>
-            <a href="#projects" onClick={toggleMenu}>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#contact" onClick={toggleMenu}>
-              Contact
-            </a>
-          </li>
-        </ul>
-        <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">
-          Chamar no WhatsApp
-        </a>
-      </div>
-
-            {/* Dropdown - Links de navegação para telas menores */}
-            {isOpen && (
-                <section className="dropdown-menu">
-                    <ul>
-                        <li><a href="#home" onClick={toggleMenu}>Home</a></li>
-                        <li><a href="#aboutme" onClick={toggleMenu}>About Me</a></li>
-                        <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
-                        <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
-                    </ul>
-                    <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">Chamar no WhatsApp</a>
-                </section>
-            )}
-
-            {/* Navbar completa para telas maiores */}
             <section className="navbar-header">
                 <div className="navbar-image-container">
                     <img src="../perfil_iguana.jpg" alt="Logo" className="navbar-image" />
-                    <div className="loader">
-                        <svg viewBox="0 0 80 80">
-                            <circle r="32" cy="40" cx="40"></circle>
-                        </svg>
-                    </div>
                 </div>
                 <div className="navbar-name">Felipe Adriano</div>
             </section>
@@ -86,7 +47,7 @@ const Navbar = () => {
             </section>
 
             <section className="navbar-whatsapp">
-                <a href="https://wa.me/seu-numerodo-whatsapp" className="whatsapp-button">Chamar no WhatsApp</a>
+                <a href="https://wa.me/47984318419" className="whatsapp-button">Chamar no WhatsApp</a>
             </section>
 
             <section className="navbar-footer">
